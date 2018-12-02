e78b31a1147b5a1e58ba5881c200b6691ddf095d
Lu Xiankai <carrierlxk@gmail.com>
2018-12-02 10:57:13 +0400

    Update my_data_augmentation.m

--- util/my_data_augmentation.m
+++ util/my_data_augmentation.m
@@ -2,15 +2,15 @@ function imgs = my_data_augmentation(img)
 %shifting_value = [100,    0, -100,    0; 0,   100,    0, -100];
 %center_img = center_crop(original_img, input_width, input_height, shifting_value(1,i), shifting_value(2,i));
 %img = imread('/home/ying/1.jpg');
-rotation_angle = [-45, -30,-20,-10,10,20,30,45];
+%rotation_angle = [-45, -30,-20,-10,10,20,30,45];
 imgs=cell(1,1);%6+flip+gaussian
 idx =0;
-for i =1:size(rotation_angle,2)
-    idx = idx+1;
-   img1 = imrotate(img,rotation_angle(i),'bicubic','crop');
-   imgs{idx,1} = img1;
+%for i =1:size(rotation_angle,2)
+%    idx = idx+1;
+%   img1 = imrotate(img,rotation_angle(i),'bicubic','crop');
+%   imgs{idx,1} = img1;
    %figure,imshow(mat2gray(img1))
-end
+%end
 
 g_sigma =[10,5,1,0.1];
 
